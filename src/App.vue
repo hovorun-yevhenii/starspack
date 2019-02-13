a02e1e03f9e362dea0ef59aea170e333d43b1b94
oxygene4 <5797565@gmail.com>
2019-02-13 23:35:01 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <app-modal></app-modal>
+  </div>
+</template>
+
+<script>
+  import AppModal from './components/AppModal'
+
+  export default {
+    name: 'app',
+    components: {
+      AppModal
+    },
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "scss/style.scss";
+
+  #app {
+    padding: 16px;
+    font-family: Arimo, sans-serif;
+    color: $clr-dark;
+  }
+
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "AppModal"
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
